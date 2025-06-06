9c00c9ed2f7a2af2150afdeb3f234d44d2f8e8f5
chenxinzhou <jojolocklock@gmail.com>
2025-06-06 13:07:07 +0800

    fix: test case

--- tests/api-resources/v1/boxes/android.test.ts
+++ tests/api-resources/v1/boxes/android.test.ts
@@ -1,6 +1,6 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-import GboxClient, { toFile } from 'gbox-sdk';
+import { GboxClient, toFile } from 'gbox-sdk';
 
 const client = new GboxClient({
   apiKey: 'My API Key',
