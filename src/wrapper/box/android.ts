c17dc8f1e38a8cf44b56525d30a87740c3618801
ProtosL <liuhmchn@163.com>
2025-06-20 14:38:47 +0800

    feat: add new android api

--- src/wrapper/box/android.ts
+++ src/wrapper/box/android.ts
@@ -10,6 +10,10 @@ import {
   AndroidListParams,
   AndroidUninstallParams,
   AndroidListResponse,
+  AndroidRotateScreenParams,
+  AndroidBackupParams,
+  AndroidListActivitiesResponse,
+  AndroidListSimpleResponse,
 } from '../../resources/v1/boxes';
 import { TimeString } from '../types';
 import { BaseBox } from './base';
@@ -79,6 +83,23 @@ export class AndroidBoxOperator extends BaseBox<AndroidBox> {
      * const response = await myBox.app.closeAll();
      */
     closeAll: (): Promise<void> => this.client.v1.boxes.android.closeAll(this.data.id),
+    /**
+     * @example
+     * const response = await myBox.app.rotateScreen({ angle: 90, direction: 'clockwise' });
+     */
+    rotateScreen: (params: AndroidRotateScreenParams): Promise<void> =>
+      this.client.v1.boxes.android.rotateScreen(this.data.id, params),
+    /**
+     * @example
+     * const response = await myBox.app.backupAll();
+     */
+    backupAll: (): Promise<Response> => this.client.v1.boxes.android.backupAll(this.data.id),
+    /**
+     * @example
+     * const response = await myBox.app.listSimple();
+     */
+    listSimple: (): Promise<AndroidListSimpleResponse> =>
+      this.client.v1.boxes.android.listSimple(this.data.id),
   };
 }
 
@@ -115,5 +136,35 @@ class AndroidAppOperator {
    */
   async close(params?: Omit<AndroidCloseParams, 'id'>) {
     return this.client.v1.boxes.android.close(this.data.packageName, { id: this.box.id, ...params });
+  }
+
+  /**
+   * @example
+   * const response = await myApp.backup();
+   */
+  async backup(params?: Omit<AndroidBackupParams, 'id'>): Promise<Response> {
+    return this.client.v1.boxes.android.backup(this.data.packageName, { id: this.box.id, ...params });
+  }
+
+  /**
+   * @example
+   * const response = await myApp.restore({
+   *   path: "path/to/your/backup.apk",
+   * });
+   */
+  async restore(params: { path: string }): Promise<void> {
+    const exists = fs.existsSync(params.path);
+    if (!exists) {
+      throw new Error(`File ${params.path} does not exist`);
+    }
+    const backupReadStream = fs.createReadStream(params.path);
+    return this.client.v1.boxes.android.restore(this.box.id, { backup: backupReadStream });
+  }
+  /**
+   * @example
+   * const response = await myApp.listActivities();
+   */
+  async listActivities(): Promise<AndroidListActivitiesResponse> {
+    return this.client.v1.boxes.android.listActivities(this.data.packageName, { id: this.box.id });
   }
 }
