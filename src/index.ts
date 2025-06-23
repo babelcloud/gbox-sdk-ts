22bd6e451ae58907db2f03563765ddcd1501747f
chenxinzhou <jojolocklock@gmail.com>
2025-06-22 18:20:04 -0700

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export { GboxSDK as default } from './wrapper/sdk';
 export { type Uploadable, toFile } from './core/uploads';
 export { APIPromise } from './core/api-promise';
 export { type ClientOptions, GboxClient } from './client';
-export { GboxSDK, type BoxOperator, type BoxListOperatorResponse } from './wrapper/sdk';
 export {
   GboxClientError,
   APIError,
@@ -21,40 +20,12 @@ export {
   PermissionDeniedError,
   UnprocessableEntityError,
 } from './core/error';
-export {
-  type AndroidBoxOperator,
-  type AndroidAppOperator,
-  type CreateAndroid,
-  type AndroidInstall,
-} from './wrapper/box/android';
-export { type LinuxBoxOperator, type CreateLinux } from './wrapper/box/linux';
-export { type TimeString, type TimeUnit } from './wrapper/types';
-export {
-  type FileSystemOperator,
-  type FileOperator,
-  type DirectoryOperator,
-} from './wrapper/box/file-system';
-export { type BrowserOperator } from './wrapper/box/browser';
-export {
-  type ActionOperator,
-  type ActionClick,
-  type ActionDrag,
-  type ActionMove,
-  type ActionScroll,
-  type ActionTouch,
-  type ActionType,
-  type ActionPressButton,
-  type ActionPressKey,
-  type ActionScreenshot,
-  type ActionSwipe,
-  type ActionSwipeSimple,
-  type ActionSwipeAdvanced,
-} from './wrapper/box/action';
-export {
-  type BoxExecuteCommands,
-  type BoxRunCode,
-  type BoxStart,
-  type BoxStop,
-  type BaseBox,
-} from './wrapper/box/base';
+export * from './wrapper/box/android';
+export * from './wrapper/box/linux';
+export * from './wrapper/types';
+export * from './wrapper/box/file-system';
+export * from './wrapper/box/browser';
+export * from './wrapper/box/action';
+export * from './wrapper/box/base';
 export * from './wrapper/utils';
+export * from './wrapper/sdk';
