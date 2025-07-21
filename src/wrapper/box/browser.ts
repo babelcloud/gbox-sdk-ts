772cf2e5a790bb002bbd41e070970eae194c4173
chenxinzhou <jojolocklock@gmail.com>
2025-07-21 10:33:07 +0800

    feat(browser): add methods for tab management and introduce BrowserTabOperator class

--- src/wrapper/box/browser.ts
+++ src/wrapper/box/browser.ts
@@ -1,3 +1,4 @@
+import { BrowserGetTabsResponse } from '../../resources/v1/boxes';
 import { GboxClient } from '../../client';
 
 export class BrowserOperator {
@@ -15,5 +16,58 @@ export class BrowserOperator {
    */
   async cdpUrl() {
     return this.client.v1.boxes.browser.cdpURL(this.boxId);
+  }
+
+  /**
+   * @example
+   * const response = await myBox.browser.listTab();
+   */
+  async listTab() {
+    const tabInfo = await this.client.v1.boxes.browser.getTabs(this.boxId);
+    return tabInfo.tabs.map((tab) => new BrowserTabOperator(this.client, this.boxId, tab));
+  }
+
+  /**
+   * @example
+   * const response = await myBox.browser.listTabInfo();
+   */
+  async listTabInfo() {
+    return this.client.v1.boxes.browser.getTabs(this.boxId);
+  }
+
+  /**
+   * @example
+   * const response = await myBox.browser.updateTab('1', 'https://www.google.com');
+   */
+  async updateTab(tabId: string, url: string) {
+    return this.client.v1.boxes.browser.updateTab(tabId, { boxId: this.boxId, url });
+  }
+
+  /**
+   * @example
+   * const response = await myBox.browser.closeTab('1');
+   */
+  async closeTab(tabId: string) {
+    return this.client.v1.boxes.browser.closeTab(tabId, { boxId: this.boxId });
+  }
+}
+
+export class BrowserTabOperator {
+  private client: GboxClient;
+  private boxId: string;
+  private data: BrowserGetTabsResponse.Tab;
+
+  constructor(client: GboxClient, boxId: string, data: BrowserGetTabsResponse.Tab) {
+    this.client = client;
+    this.boxId = boxId;
+    this.data = data;
+  }
+
+  /**
+   * @example
+   * const response = await myBox.browser.tab.update('https://www.google.com');
+   */
+  async update(url: string) {
+    return this.client.v1.boxes.browser.updateTab(this.data.id, { boxId: this.boxId, url });
   }
 }
